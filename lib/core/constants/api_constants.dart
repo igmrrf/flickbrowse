3e3d2be7282ce3e6a59e27011024239e80953e4a
Igbiriki Francis <francis.igbiriki@gmail.com>
2025-03-27 21:12:51 +0100

    remove env

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -5,7 +5,7 @@ class ApiConstants {
   static const String backdropSize = 'original';
 
   // Add your TMDB API key here
-  static const String apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGEwMTJjZjBmNWJjZTJmMTIzZTVkYTUyMjNkM2QwMCIsIm5iZiI6MTc0MzEwNDk0Ny41NDIsInN1YiI6IjY3ZTVhYmIzNWYzZTBhYzE4ODAwM2E3OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ngzQDlHzMrWcpkiV48NSWCUywnwxcLWy74lpCEgEq3E';
+  static const String apiKey = 'apikey';
 
   static String getPosterUrl(String path) => '$imageBaseUrl/$posterSize$path';
   static String getBackdropUrl(String path) =>
